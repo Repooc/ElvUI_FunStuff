890c9fb7b8b1ba2887e43f9712b9194481142673
Repooc <tjcooper1982@gmail.com>
2022-04-30 15:55:47 -0400

    toc & cl

--- ElvUI_FunStuff/Modules/Changelog.lua
+++ ElvUI_FunStuff/Modules/Changelog.lua
@@ -6,6 +6,10 @@ local module = E:NewModule('FunStuff-Changelog', 'AceEvent-3.0', 'AceTimer-3.0')
 local format, gsub, find = string.format, string.gsub, string.find
 
 local ChangelogTBL = {
+	'v1.02 4/30/2022',
+		"• Update Tukui Player profile a little",
+		"• Bump SoM TOC",
+	' ',
 	'v1.01 4/8/2022',
 		"• Update profiles",
 	' ',
