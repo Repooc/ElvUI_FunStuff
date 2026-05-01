70acb785fba15992a2de6236720ea7b28777a90b
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2026-05-01 10:17:29 +0300

    Update ruRU.lua

--- ElvUI_FunStuff/Locales/ruRU.lua
+++ ElvUI_FunStuff/Locales/ruRU.lua
@@ -1,41 +1,41 @@
 -- Russian localization file for ruRU.
 local L = ElvUI[1].Libs.ACL:NewLocale("ElvUI", "ruRU")
-
-L["|cffFF3333Warning:|r You already have a profile named, |cffFFD100%s|r.\n\nClick \"Overwrite\" to overwrite the profile or click \"Cancel\" to cancel the operation."] = true
-L["Apply To Current Profile"] = true
-L["Choose Layout"] = true
-L["Configure Layout"] = true
-L["Create New Profile"] = true
-L["Curseforge Page"] = true
-L["Development Version"] = true
-L["Do you enjoy the new ElvUI?"] = true
-L["Download"] = true
-L["ElvUI needs to perform database optimizations please be patient."] = true
-L["FUN_CREDITS"] = "Just a want to give credit to The following people for helping me maintain this addon with testing, coding, textures and people who also have helped me through donations. Please note for donation acknowledgments, only the names of people who messaged me or noted in the donation will be listed as you ask (within reason of course)."
-L["FUNSTUFF_INSTALL_WELCOME"] = 'This installer will let you setup previous April Fools jokes ElvUI has had in the past years. Since there has been a decent amount of requests to have them available to use, this project has emerged for everyone to enjoy them once again or even for the first time ever for you new comers to ElvUI or WoW in general!'
-L["Help Testing Development Versions"] = true
-L["Help"] = true
-L["If you are in a rush for some dancing kittys and don't feel like going through the config, the button below will allow you to quickly enable them so you can get back to what you were doing!"] = true
-L["If you choose to not import any of the profiles for now, you may click the \"Skip Process\" button to close the installation wizard."] = true
-L["If you decided to not create a new profile or apply the whole Hello Kitty theme, you can always have just the dancing kittys instead!"] = true
-L["Link to the latest development version."] = true
-L["Misc Options"] = true
-L["Please press the continue button to go onto the next step."] = true
-L["Profile Imported"] = true
-L["Profile Updated"] = true
-L["Read Before Clicking!!!"] = true
-L["Setup Kitty Layout"] = true
-L["Setup Tukui Layout"] = true
-L["Skip Process"] = true
-L["Textures/Coding"] = true
-L["Toggle Dancing Kittys!"] = true
-L["Type /hellokitty to revert to old settings."] = true
-L["Wago Page"] = true
-L["Yes, Keep Changes!"] = true
-L["You are now finished with the installation process. If you are in need of technical support please visit https://github.com/Repooc/ElvUI_FunStuff."] = true
-L["You can choose between |cffFF8000\"Tukui\"|r and |cff1784d1\"Hello Kitty\"|r layouts."] = true
-L["You can choose to \"|cff1784d1Create New Profile|r\" which will create a new profile labeled as \"%s v|cff1784d1%s|r\"."] = true
-L["You can click the previous button below to go back and choose a layout/theme, or click continue without importing one."] = true
+-- Translator ZamestoTV
+L["|cffFF3333Warning:|r You already have a profile named, |cffFFD100%s|r.\n\nClick \"Overwrite\" to overwrite the profile or click \"Cancel\" to cancel the operation."] = "|cffFF3333Внимание:|r профиль с именем |cffFFD100%s|r уже существует.\n\nНажмите «Перезаписать», чтобы заменить его, или «Отмена», чтобы прервать операцию."
+L["Apply To Current Profile"] = "Применить к текущему профилю"
+L["Choose Layout"] = "Выбрать макет"
+L["Configure Layout"] = "Настроить макет"
+L["Create New Profile"] = "Создать новый профиль"
+L["Curseforge Page"] = "Страница на Curseforge"
+L["Development Version"] = "Версия для разработки"
+L["Do you enjoy the new ElvUI?"] = "Вам нравится новый ElvUI?"
+L["Download"] = "Скачать"
+L["ElvUI needs to perform database optimizations please be patient."] = "ElvUI необходимо оптимизировать базу данных, пожалуйста, подождите."
+L["FUN_CREDITS"] = "Я хочу выразить благодарность следующим людям за помощь в поддержке этого аддона: за тестирование, программирование, текстуры, а также тем, кто помогал пожертвованиями. Обратите внимание, что в списке донатов указываются только те имена, которые были указаны в сообщении к переводу (в пределах разумного, разумеется)."
+L["FUNSTUFF_INSTALL_WELCOME"] = "Этот установщик позволит вам настроить старые первоапрельские шутки, которые были в ElvUI в прошлые годы. Поскольку было много просьб сделать их доступными, появился этот проект, чтобы каждый мог насладиться ими снова (или впервые, если вы новичок в ElvUI или WoW)!"
+L["Help Testing Development Versions"] = "Помощь в тестировании версий для разработки"
+L["Help"] = "Помощь"
+L["If you are in a rush for some dancing kittys and don't feel like going through the config, the button below will allow you to quickly enable them so you can get back to what you were doing!"] = "Если вам нетерпится увидеть танцующих котиков и не хочется копаться в настройках, кнопка ниже позволит быстро включить их, чтобы вы могли вернуться к своим делам!"
+L["If you choose to not import any of the profiles for now, you may click the \"Skip Process\" button to close the installation wizard."] = "Если вы решите пока не импортировать профили, нажмите кнопку «Пропустить», чтобы закрыть мастер установки."
+L["If you decided to not create a new profile or apply the whole Hello Kitty theme, you can always have just the dancing kittys instead!"] = "Если вы решили не создавать новый профиль и не применять тему Hello Kitty целиком, вы всегда можете включить только танцующих котиков!"
+L["Link to the latest development version."] = "Ссылка на последнюю версию для разработки."
+L["Misc Options"] = "Разные настройки"
+L["Please press the continue button to go onto the next step."] = "Пожалуйста, нажмите кнопку «Далее», чтобы перейти к следующему шагу."
+L["Profile Imported"] = "Профиль импортирован"
+L["Profile Updated"] = "Профиль обновлен"
+L["Read Before Clicking!!!"] = "Прочтите перед нажатием!!!"
+L["Setup Kitty Layout"] = "Настроить макет Kitty"
+L["Setup Tukui Layout"] = "Настроить макет Tukui"
+L["Skip Process"] = "Пропустить"
+L["Textures/Coding"] = "Текстуры / Код"
+L["Toggle Dancing Kittys!"] = "Вкл/Выкл танцующих котиков!"
+L["Type /hellokitty to revert to old settings."] = "Введите /hellokitty, чтобы вернуть старые настройки."
+L["Wago Page"] = "Страница на Wago"
+L["Yes, Keep Changes!"] = "Да, сохранить изменения!"
+L["You are now finished with the installation process. If you are in need of technical support please visit https://github.com/Repooc/ElvUI_FunStuff."] = "Процесс установки завершен. Если вам нужна техническая поддержка, посетите https://github.com/Repooc/ElvUI_FunStuff."
+L["You can choose between |cffFF8000\"Tukui\"|r and |cff1784d1\"Hello Kitty\"|r layouts."] = "Вы можете выбрать между макетами |cffFF8000«Tukui»|r и |cff1784d1«Hello Kitty»|r."
+L["You can choose to \"|cff1784d1Create New Profile|r\" which will create a new profile labeled as \"%s v|cff1784d1%s|r\"."] = "Вы можете выбрать «|cff1784d1Создать новый профиль|r», что создаст профиль с названием «%s v|cff1784d1%s|r»."
+L["You can click the previous button below to go back and choose a layout/theme, or click continue without importing one."] = "Вы можете нажать кнопку «Назад», чтобы вернуться и выбрать макет/тему, или нажать «Далее» без импорта."
 
 --! Do not translate as they are in ElvUI Locales already
 -- L["Color"]
